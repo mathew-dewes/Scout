ea7794de639cc4add08126e9afe43253a9b81b5e
Mathew Dewes <mathewdewes01@gmail.com>
2025-12-29 09:49:23 +1300

    Convex added and initialized

--- app/(shared-layout)/page.tsx
+++ app/(shared-layout)/page.tsx
@@ -5,6 +5,7 @@ export default function HomePage(){
   return (
     <div>
         <Link className={buttonVariants()} href={'/'}>Hello World</Link>
+        <p className="mt-5">Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum, vero? Debitis eius accusantium aliquid facilis necessitatibus rerum excepturi nihil aut!</p>
     </div>
   )
 }
